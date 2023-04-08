e579d6c0a06bc094b3991a02603b0c9e2a56ca24
protobj <3146330150@qq.com>
2023-04-08 14:55:35 +0800

    Transport

---
+++ game/src/main/java/transport/tcp/TcpSender.java
@@ -0,0 +1,57 @@
+package transport.tcp;
+
+import io.netty.channel.ChannelOption;
+import io.scalecube.net.Address;
+import reactor.core.publisher.Mono;
+import reactor.netty.Connection;
+import reactor.netty.tcp.TcpClient;
+import transport.Message;
+import transport.Sender;
+import transport.TransportConfig;
+import transport.TransportImpl;
+
+public final class TcpSender implements Sender {
+
+  private final TransportConfig config;
+
+  TcpSender(TransportConfig config) {
+    this.config = config;
+  }
+
+  @Override
+  public Mono<Connection> connect(Address address) {
+    return Mono.deferContextual(context -> Mono.just(context.get(TransportImpl.SenderContext.class)))
+        .map(context -> newTcpClient(context, address))
+        .flatMap(TcpClient::connect);
+  }
+
+  @Override
+  public Mono<Void> send(Message message) {
+    return Mono.deferContextual(
+        context -> {
+          Connection connection = context.get(Connection.class);
+          TransportImpl.SenderContext senderContext = context.get(TransportImpl.SenderContext.class);
+          return connection
+              .outbound()
+              .sendObject(Mono.just(message).map(senderContext.messageEncoder()), bb -> true)
+              .then();
+        });
+  }
+
+  private TcpClient newTcpClient(TransportImpl.SenderContext context, Address address) {
+    TcpClient tcpClient =
+        TcpClient.newConnection()
+            .runOn(context.loopResources())
+            .host(address.host())
+            .port(address.port())
+            .option(ChannelOption.TCP_NODELAY, true)
+            .option(ChannelOption.SO_KEEPALIVE, true)
+            .option(ChannelOption.SO_REUSEADDR, true)
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, config.connectTimeout())
+            .doOnChannelInit(
+                (connectionObserver, channel, remoteAddress) ->
+                    new TcpChannelInitializer(config.maxFrameLength())
+                        .accept(connectionObserver, channel));
+    return config.isClientSecured() ? tcpClient.secure() : tcpClient;
+  }
+}
---
+++ game/src/main/java/transport/websocket/WebsocketReceiver.java
@@ -0,0 +1,51 @@
+package transport.websocket;
+
+import io.netty.channel.ChannelOption;
+import io.scalecube.cluster.transport.api.TransportConfig;
+import io.scalecube.transport.netty.Receiver;
+import io.scalecube.transport.netty.TransportImpl.ReceiverContext;
+import reactor.core.publisher.Mono;
+import reactor.netty.DisposableServer;
+import reactor.netty.http.server.HttpServer;
+import reactor.netty.http.server.HttpServerRequest;
+import reactor.netty.http.server.HttpServerResponse;
+import reactor.netty.http.websocket.WebsocketInbound;
+import reactor.netty.http.websocket.WebsocketOutbound;
+
+public final class WebsocketReceiver implements Receiver {
+
+  private final TransportConfig config;
+
+  public WebsocketReceiver(TransportConfig config) {
+    this.config = config;
+  }
+
+  @Override
+  public Mono<DisposableServer> bind() {
+    return Mono.deferContextual(context -> Mono.just(context.get(ReceiverContext.class)))
+        .flatMap(
+            context ->
+                newHttpServer(context)
+                    .handle((request, response) -> onMessage(context, request, response))
+                    .bind()
+                    .cast(DisposableServer.class));
+  }
+
+  private HttpServer newHttpServer(ReceiverContext context) {
+    return HttpServer.create()
+        .runOn(context.loopResources())
+        .port(config.port())
+        .childOption(ChannelOption.TCP_NODELAY, true)
+        .childOption(ChannelOption.SO_KEEPALIVE, true)
+        .childOption(ChannelOption.SO_REUSEADDR, true);
+  }
+
+  private Mono<Void> onMessage(
+      ReceiverContext context,
+      @SuppressWarnings("unused") HttpServerRequest request,
+      HttpServerResponse response) {
+    return response.sendWebsocket(
+        (WebsocketInbound inbound, WebsocketOutbound outbound) ->
+            inbound.receive().retain().doOnNext(context::onMessage).then());
+  }
+}
